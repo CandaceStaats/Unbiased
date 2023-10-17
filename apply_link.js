e80ef7add683fbc8f6860b56b0e5a5c9e367e2ef
Douwe <douwe@Ubuntu.myguest.virtualbox.org>
2023-10-17 01:17:31 -0700

    added cool documentation for apply link

---
+++ apply_link.js
@@ -0,0 +1,8 @@
+/*
+In the front end, a form has been filled in by a hiring manager,
+specifying the position they are hiring for, the company they are representing and their email.
+We are using RestAPI to request this information.
+In the function, we take this and encrypt it into a /apply link, which the hiring manager
+posts on their website. We send this link back, probably directly on the front end.
+If that turns out harder than imagined, however, we can also send it to the email
+*/
