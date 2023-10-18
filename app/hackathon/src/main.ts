f1632bad4f78aa3f44074a47a2f70f21b8676afb
Jeroen Van Halderen <jvan-hal@f1r5s10.codam.nl>
2023-10-18 10:21:10 +0200

    try to make openapi documentation

--- app/hackathon/src/main.ts
+++ app/hackathon/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	const config = new DocumentBuilder()
+		.setTitle('Unbiased backend')
+		.setDescription('The unbiased backend API description')
+		.setVersion('0.1')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+	await app.listen(3000);
 }
 bootstrap();
