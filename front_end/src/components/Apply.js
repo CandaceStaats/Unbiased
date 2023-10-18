36abaf99b3c0d5e4a2276cbdc1675b43ca4df6d6
dmaessen <119501224+dmaessen@users.noreply.github.com>
2023-10-18 16:46:52 +0200

    frontend documentation updated + apply form finalised (#10)
    Co-authored-by: domi <dmaessen@student.codam.nl>

--- front_end/src/components/Apply.js
+++ front_end/src/components/Apply.js
@@ -4,40 +4,74 @@ import {useParams} from 'react-router-dom'
 import axios from 'axios';
 
 function Apply() {
-	  
+	
+	const [isSuccess, setIsSuccess] = useState(true)
 	const [name, setName] = useState("")
 	const [email, setEmail] = useState("")
 	const [cv, setCv] = useState(null)
+	const [nickName, setNickName] = useState("")
+	const [profile, setProfile] = useState("")
+	const [career, setCareer] = useState("")
+	const [work, setWork] = useState(false)
+	const [skills, setSkills] = useState(false)
+	const [certificates, setCertificates] = useState(false)
+	const [education, setEducation] = useState(false)
+	const [achivements, setAchivements] = useState(false)
 	const [jobTitle, setJobTitle] = useState("Job title")
 	const [companyName, setCompanyName] = useState("Company name")
 	const [isNameActive, setIsNameActive] = useState(false)
 	const [isEmailActive, setIsEmailActive] = useState(false)
-	const [isNickName, setisNickName] = useState(false)
-	const [isProfile, setisProfile] = useState(false)
-	const [isCareer, setisCareer] = useState(false)
-	const [isWork, setisWork] = useState(false)
-	const [isSkills, setisSkills] = useState(false)
-	const [isCertificates, setisCertificates] = useState(false)
-	const [isEducation, setisEducation] = useState(false)
-	const [isAchivements, setisAchivements] = useState(false)
-	const [isSuccess, setIsSuccess] = useState(false)
+	const [isNickNameActive, setIsNickNameActive] = useState(false)
+	const [isProfileActive, setIsProfileActive] = useState(false)
+	const [isCareerActive, setIsCareerActive] = useState(false)
+	const [isWorkActive, setIsWorkActive] = useState(false)
+	const [isSkillsActive, setIsSkillsActive] = useState(false)
+	const [isCertificatesActive, setIsCertificatesActive] = useState(false)
+	const [isEducationActive, setIsEducationActive] = useState(false)
+	const [isAchivementsActive, setIsAchivementsActive] = useState(false)
 
 	let {jobToken} = useParams()
 	useEffect(() => {
 		console.log(jobToken)
-		axios.get(`api/get/${jobToken}`)
+		axios.get(`https://localhost:3333/apply/${jobToken}`)
 		.then(res => {
-			setJobTitle(res.title)
-			setCompanyName(res.name)
+			console.log(res)
+			setJobTitle(res.title) // adapt name to match backend
+			setCompanyName(res.name) // adapt name to match backend
 		})
 		.catch(err => {
-		setIsSuccess(false)
-	})
+		setIsSuccess(false) // set to (false) to show the form, else set to (true) to hide the form
+		})
 	});
+
+	function onFormSubmit(e){
+		e.preventDefault();
+	if (name === "" || email === "" || cv === null)
+	{
+		console.log("please fill in all the fields")
+	}
+		let formUpload={
+			nickName: nickName,
+			profile: profile,
+			career: career,
+			work: work,
+			skills: skills,
+			certificates: certificates,
+			education: education,
+			achivements: achivements
+		}
+		axios.post(`https://localhost:3333/apply/form`, { formUpload })
+			.then(res => {
+				console.log(res)
+				console.log(res.data)
+			})
+			.catch(err => {
+			})
+	};
 
 	function onSubmit(e){
 		e.preventDefault();
-	if (name == "" || email == "" || cv == null)
+	if (name === "" || email === "" || cv === null)
 	{
 		console.log("please fill in all the fields")
 	}
@@ -94,21 +128,97 @@ function Apply() {
 		</form>
 	</div>
 		{isSuccess ? <h1></h1> :
-		
-		<div className="inner-container">
-		<form>
-			<div className={`ID1_applicant input-containe ${isNameActive ? 'active' : ''}`}>
-				<input
-				className="input-place"
-				type="text"
-				onFocus={() => setIsNameActive(true)}
-				onBlur={() => setIsNameActive(false)}
-				onChange={(e) => setName(e.target.value)}
-				required
-				/>
-				<label>Nick name</label>
-			</div>
-		</form>
+		<div className="inner-container"> 
+			<form onSubmit={e => {onFormSubmit(e)}}>
+				<div className={`ID3_applicant input-containe ${isNickNameActive ? 'active' : ''}`}>
+					<input
+					className="input-place"
+					type="text"
+					onFocus={() => setIsNickNameActive(true)}
+					onBlur={() => setIsNickNameActive(false)}
+					onChange={(e) => setNickName(e.target.value)}
+					required
+					/>
+					<label>Nick name</label>
+				</div>
+				<div className={`ID4_applicant input-containe ${isProfileActive ? 'active' : ''}`}>
+					<input
+					className="input-place"
+					type="text"
+					onFocus={() => setIsProfileActive(true)}
+					onBlur={() => setIsProfileActive(false)}
+					onChange={(e) => setProfile(e.target.value)}
+					required
+					/>
+					<label>Profile - Personal description</label>
+				</div>
+				<div className={`ID5_applicant input-containe ${isCareerActive ? 'active' : ''}`}>
+					<input
+					className="input-place"
+					type="text"
+					onFocus={() => setIsCareerActive(true)}
+					onBlur={() => setIsCareerActive(false)}
+					onChange={(e) => setCareer(e.target.value)}
+					required
+					/>
+					<label>Career Profile</label>
+				</div>
+				<div className={`ID6_applicant input-containe ${isWorkActive ? 'active' : ''}`}>
+					<input
+					className="input-place"
+					type="text"
+					onFocus={() => setIsWorkActive(true)}
+					onBlur={() => setIsWorkActive(false)}
+					onChange={(e) => setWork(e.target.value)}
+					required
+					/>
+					<label>Work Experience</label>
+				</div>
+				<div className={`ID7_applicant input-containe ${isSkillsActive ? 'active' : ''}`}>
+					<input
+					className="input-place"
+					type="text"
+					onFocus={() => setIsSkillsActive(true)}
+					onBlur={() => setIsSkillsActive(false)}
+					onChange={(e) => setSkills(e.target.value)}
+					required
+					/>
+					<label>Skills</label>
+				</div>
+				<div className={`ID8_applicant input-containe ${isCertificatesActive ? 'active' : ''}`}>
+					<input
+					className="input-place"
+					type="text"
+					onFocus={() => setIsCertificatesActive(true)}
+					onBlur={() => setIsCertificatesActive(false)}
+					onChange={(e) => setCertificates(e.target.value)}
+					required
+					/>
+					<label>Certificates</label>
+				</div>
+				<div className={`ID9_applicant input-containe ${isEducationActive ? 'active' : ''}`}>
+					<input
+					className="input-place"
+					type="text"
+					onFocus={() => setIsEducationActive(true)}
+					onBlur={() => setIsEducationActive(false)}
+					onChange={(e) => setEducation(e.target.value)}
+					required
+					/>
+					<label>Education</label>
+				</div>
+				<div className={`ID10_applicant input-containe ${isAchivementsActive ? 'active' : ''}`}>
+					<input
+					className="input-place"
+					type="text"
+					onFocus={() => setIsAchivementsActive(true)}
+					onBlur={() => setIsAchivementsActive(false)}
+					onChange={(e) => setAchivements(e.target.value)}
+					required
+					/>
+					<label>Achivements</label>
+				</div>
+			</form>
 		</div>}
 	 </div>
   );
--- frontend_doc.md
+++ frontend_doc.md
@@ -17,6 +17,11 @@ Then the person will be asked to fill in there full name + their email (this inf
 and their email will be used for the communication between the HR-manager and the applicant (email comms will always go through the unbiased email system, hiding their emails).
 Then they’ll need to upload their CV in PDF format. They’ll be able to click the “Submit” button with a capcha pop up for their application to be processed and sent to the HR-manager.
 Then the applicant will see a “success” pop up for a “fail” pop up, to indicate if all went well.
+In case the applicant can't/doesn't want to upload its CV in PDF format, there is the possibility to fill out a form with a set of fields -that only appears upon request- (nickname, profile, career profile, work experience, skills, certificates, education, achievements).
 ## INFO REQUIRED ON THE PAGE:
  (displayed): Company name and job title (filled in based on the url’s encryption).
  (to be filled): Full name, email of the applicant, upload CV button, and submit button.
+
+
+# Approved design mockups on Figma
+https://www.figma.com/file/Aev5dxSM8k9dhnT6FHHgoi/adaptive-test?type=design&node-id=0%3A1&mode=design&t=rA7gcQyN0nrCu7me-1
